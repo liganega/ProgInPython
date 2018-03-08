575343ad822e11843743ebc2790e7c221fc98c0a
liganega <gslee@hknu.ac.kr>
2018-03-08 15:41:01 +0900

    ch01 코드 추가

---
+++ codes/ch01/ch01_a.py
@@ -0,0 +1,11 @@
+print("Welcome")
+g = input("Guess the number: ")
+guess = int(g)
+if guess == 5:
+    print("You win")
+else:
+    if guess > 5:
+        print("Too high")
+    else:
+        print("Too low")
+print("Game over!")
