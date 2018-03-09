b9cbd0016e1f51dcecc294012335382d251d3572
Gyesik Lee <gslee@hknu.ac.kr>
2018-03-09 10:55:22 +0900

    ch01 코드 추가

---
+++ codes/ch01/ch01b.py
@@ -0,0 +1,14 @@
+from random import randint 
+secret = randint(1, 10) 
+
+print("Welcome!") 
+g = input("Guess the number: ") 
+guess = int(g) 
+if guess == secret: 
+    print("You win!") 
+else: 
+    if guess > secret: 
+        print("Too high") 
+    else: 
+        print("Too low") 
+print("Game over!")
